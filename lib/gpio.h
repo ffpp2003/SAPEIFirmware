2d97d54e5d1aac4a4ef24f0df7bf534202af2b44
ffpp2003 <ffpp.2003@hotmail.com>
2024-10-21 01:43:14 -0300

    Autoasignacion de registros. Migracion a avr-gcc

--- lib/gpio.h
+++ lib/gpio.h
@@ -1,5 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#include <avr/io.h>
 
 /**
  * @brief Estructura de bits de un byte.
@@ -28,33 +29,33 @@ typedef union {
 
 } bitreg_u;
 
-extern bitreg_u PORTb_bitreg; /// Registro personalizado para PORTB.
+bitreg_u PORTb_bitreg = {.REG = &PORTB}; /// Registro personalizado para PORTB.
 #define PORTb *(PORTb_bitreg.REG)
 #define PORTbBits (*(PORTb_bitreg.bits))
-extern bitreg_u PINb_bitreg; ///Registro personalizado para PINB.
+bitreg_u PINb_bitreg = {.REG = &PINB}; /// Registro personalizado para PINB.
 #define PINb *(PINb_bitreg.REG)
 #define PINbBits (*(PINb_bitreg.bits))
-extern bitreg_u DDRb_bitreg; ///Registro personalizado para DDRB.
+bitreg_u DDRb_bitreg = {.REG = &DDRB}; /// Registro personalizado para DDRB.
 #define DDRb *(DDRb_bitreg.REG)
 #define DDRbBits (*(DDRb_bitreg.bits))
 
-extern bitreg_u PORTc_bitreg; ///Registro personalizado para PORTC.
+bitreg_u PORTc_bitreg = {.REG = &PORTC}; /// Registro personalizado para PORTC.
 #define PORTc *(PORTc_bitreg.REG)
 #define PORTcBits (*(PORTc_bitreg.bits))
-extern bitreg_u PINc_bitreg; ///Registro personalizado para PINC.
+bitreg_u PINc_bitreg = {.REG = &PINC}; /// Registro personalizado para PINC.
 #define PINc *(PINc_bitreg.REG)
 #define PINcBits (*(PINc_bitreg.bits))
-extern bitreg_u DDRc_bitreg; ///Registro personalizado para DDRC.
+bitreg_u DDRc_bitreg = {.REG = &DDRC}; /// Registro personalizado para DDRC.
 #define DDRc *(DDRc_bitreg.REG)
 #define DDRcBits (*(DDRc_bitreg.bits))
 
-extern bitreg_u PORTd_bitreg; ///Registro personalizado para PORTD.
+bitreg_u PORTd_bitreg = {.REG = &PORTD}; /// Registro personalizado para PORTD.
 #define PORTd *(PORTd_bitreg.REG)
 #define PORTdBits (*(PORTd_bitreg.bits))
-extern bitreg_u PINd_bitreg; ///Registro personalizado para PINC.
+bitreg_u PINd_bitreg = {.REG = &PIND}; /// Registro personalizado para PINC.
 #define PINd *(PINd_bitreg.REG)
 #define PINdBits (*(PINd_bitreg.bits))
-extern bitreg_u DDRd_bitreg; ///Registro personalizado para DDRD.
+bitreg_u DDRd_bitreg = {.REG = &DDRD}; /// Registro personalizado para DDRD.
 #define DDRd *(DDRd_bitreg.REG)
 #define DDRdBits (*(DDRd_bitreg.bits))
 
