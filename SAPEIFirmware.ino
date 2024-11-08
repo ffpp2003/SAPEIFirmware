7ed125dddbde0df895a87355a2b50da9948aa880
ffpp2003 <ffpp.2003@hotmail.com>
2024-11-08 16:05:16 -0300

    Sustituye el control de las salidas por ISR TMR1

--- SAPEIFirmware.ino
+++ SAPEIFirmware.ino
@@ -11,21 +11,45 @@
 #define GRN_LED PORTdBits.b7
 #define BUZZER PORTbBits.b0
 
-#define LEDS_PORT PORTD
-#define BUZZER_PORT PORTB
+#define LEDS_PORT &PORTD
+#define BUZZER_PORT &PORTB
 
 #define RED 5
 #define YEL 6
 #define GRN 7
 #define BZZ 0
 
+#define SIMULTANEOUS_CONTROL_NUMBER 2
+
+#define TIMER_INTERRUPT_DEBUG         0
+#define _TIMERINTERRUPT_LOGLEVEL_     0
+#define USE_TIMER_1 true
+
+#include "TimerInterrupt.h"
+
 MFRC522DriverPinSimple ss_1_pin(10);
 MFRC522DriverSPI driver_1{ss_1_pin};
 MFRC522 reader{driver_1};
 
-void blink(uint8_t *port, uint8_t pin, uint16_t period);
-void flash(uint8_t *port, uint8_t pin, uint16_t onTime, uint16_t period);
-void manageFeedback(uint16_t code = 0xffff);
+void blink(uint8_t channel, uint8_t *port, uint8_t pin, uint16_t period, uint8_t repetitions = 0);
+void flash(uint8_t channel, uint8_t *port, uint8_t pin, uint16_t period, uint16_t onTime, uint8_t repetitions = 0);
+void pinsControlHandler();
+void pinControlHandler(uint8_t channel);
+void pinControl(uint8_t channel, uint8_t *port, uint8_t pin, uint8_t type, uint8_t repetitions, uint16_t period, uint16_t onTime = 0);
+
+typedef struct{
+  volatile bool alreadyControlling;
+  volatile uint8_t *conPort;
+  volatile uint8_t conPin;
+  volatile uint8_t conType;
+  volatile uint8_t conRepetitions;
+  volatile uint16_t conPeriod;
+  volatile uint16_t conOnTime;
+  volatile uint16_t passes;
+  volatile uint16_t repetitions;
+} pinStatus_s;
+
+pinStatus_s pins[SIMULTANEOUS_CONTROL_NUMBER];
 
 void setup(){
   Serial.begin(115200);
@@ -33,6 +57,12 @@ void setup(){
   DDRb |= _BV(PORTB0);
 
   Serial.print("INIT ");
+
+  ITimer1.init();
+  if (!ITimer1.attachInterruptInterval(10, pinsControlHandler)){
+    Serial.println("FAIL");
+    while(1);
+  }
 
   if(reader.PCD_Init() == 1){
     Serial.println("OK");
@@ -51,7 +81,7 @@ void setup(){
       else
         memset(rxbuff, 0, 50);
     }
-    flash(&PORTD, 5, 50, 1000);
+    flash(0, LEDS_PORT, RED, 1000, 50);
   }
   Serial.println("CONNECTED");
 }
@@ -67,87 +97,94 @@ void loop(){
     readerTimestamp = millis();
   }
 
-  if(Serial.available()){
+  if (Serial.available()){
     char rxbuff[5] = {0};
     Serial.readBytesUntil('\n', rxbuff, 5);
-    manageFeedback(atoi(rxbuff));
+    uint16_t code = atoi(rxbuff);
+    switch (code){
+        case OK:
+            blink(0, LEDS_PORT, GRN, 500, 10);
+            flash(1, BUZZER_PORT, BZZ, 100, 75, 1);
+            break;
+        case TR_NOT_ENOUGH_FUNDS:
+            blink(0, LEDS_PORT, RED, 500, 10);
+            flash(1, BUZZER_PORT, BZZ, 500, 300, 3);
+            break;
+        case TR_JUST_ENOUGH_FUNDS:
+            blink(0, LEDS_PORT, YEL, 500, 10);
+            flash(1, BUZZER_PORT, BZZ, 100, 75, 3);
+            break;
+        case TR_CLIENT_NOT_FOUND:
+            flash(0, LEDS_PORT, RED, 100, 50, 25);
+            flash(1, BUZZER_PORT, BZZ, 100, 50, 15);
+            break;
+        default:
+            break;
+    }
   }
-  manageFeedback();
 }
 
-void blink(uint8_t *port, uint8_t pin, uint16_t period){
-    static uint32_t timestamp;
-    if (millis() >= timestamp + period){
-        *port &= ~_BV(pin);
-        timestamp = millis();
+void blink(uint8_t channel, uint8_t *port, uint8_t pin, uint16_t period, uint8_t repetitions){
+    pinControl(channel, port, pin, 1, repetitions, period);
+}
+
+void flash(uint8_t channel, uint8_t *port, uint8_t pin, uint16_t period, uint16_t onTime, uint8_t repetitions){
+    pinControl(channel, port, pin, 2, repetitions, period, onTime);
+}
+
+void pinControlHandler(uint8_t channel){ // ISR ONLY!!!
+  if(pins[channel].alreadyControlling == 1){
+    switch (pins[channel].conType){
+      case 1: // blink
+        if (pins[channel].passes < pins[channel].conPeriod/2)
+          *(pins[channel].conPort) |= _BV(pins[channel].conPin);
+        else
+          *(pins[channel].conPort) &= ~_BV(pins[channel].conPin);
+        break;
+      case 2: // flash
+        if (pins[channel].passes < pins[channel].conOnTime)
+          *(pins[channel].conPort) |= _BV(pins[channel].conPin);
+        else
+          *(pins[channel].conPort) &= ~_BV(pins[channel].conPin);
+        break;
     }
-    if (millis() >= timestamp + period/2)
-        *port |= _BV(pin);
-}
-
-void flash(uint8_t *port, uint8_t pin, uint16_t onTime, uint16_t period){
-    static uint32_t timestamp;
-    if (millis() >= timestamp + period)
-        timestamp = millis();
-    if (millis() >= timestamp + onTime){
-        *port &= ~_BV(pin);
+    if (pins[channel].passes >= pins[channel].conPeriod){
+      if (pins[channel].repetitions >= pins[channel].conRepetitions){
+        pins[channel].alreadyControlling = 0;
+        pins[channel].conPort == NULL;
+        pins[channel].conPin = 0;
+        pins[channel].conType = 0;
+        pins[channel].conPeriod = 0;
+        pins[channel].conOnTime = 0;
+        pins[channel].conRepetitions = 0;
+        pins[channel].passes = 0;
+        pins[channel].repetitions = 0;
+      } else {
+        pins[channel].repetitions++;
+        pins[channel].passes = 0;
+      }
     } else {
-        *port |= _BV(pin);
+      pins[channel].passes++;
     }
+  }
 }
 
-void manageFeedback(uint16_t code){
-    static uint32_t timestamp;
-    static uint8_t mode = 0, led = 0;
+void pinControl(uint8_t channel, uint8_t *port, uint8_t pin, uint8_t type, uint8_t repetitions, uint16_t period, uint16_t onTime){
+  if(pins[channel].alreadyControlling == 0){
+    pins[channel].conPort = port;
+    pins[channel].conPin = pin;
+    pins[channel].conType = type;
+    pins[channel].conPeriod = period/10;
+    pins[channel].conOnTime = onTime/10;
+    pins[channel].conType = type;
+    pins[channel].conRepetitions = repetitions;
+    pins[channel].repetitions = 0;
+    pins[channel].passes = 0;
+    pins[channel].alreadyControlling = 1;
+  }
+}
 
-    if (code == 0xffff){
-        switch (mode){
-            case 0:
-                RED_LED = YEL_LED = GRN_LED = BUZZER = 0;
-                break;
-            case 1:
-                blink(&LEDS_PORT, led, 500);
-                flash(&BUZZER_PORT, BZZ, 50, 5500);
-                break;
-            case 2:
-                flash(&BUZZER_PORT, BZZ, 850, 5500);
-                break;
-            default:
-                break;
-        }
-        if (millis() >= timestamp + 5000){
-            timestamp = millis();
-            mode = 0;
-        }
-    } else {
-        switch (code){
-            case OK:
-                mode = 1;
-                GRN_LED = 1;
-                //BUZZER = 1;
-                led = GRN;
-                break;
-            case TR_NOT_ENOUGH_FUNDS:
-                mode = 1;
-                RED_LED = 1;
-                //BUZZER = 1;
-                led = RED;
-                break;
-            case TR_JUST_ENOUGH_FUNDS:
-                mode = 1;
-                YEL_LED = 1;
-                //BUZZER = 1;
-                led = YEL;
-                break;
-            case TR_CLIENT_NOT_FOUND:
-                mode = 2;
-                RED_LED = 1;
-                //BUZZER = 1;
-                led = RED;
-                break;
-            default:
-                break;
-        }
-        timestamp = millis();
-    }
+void pinsControlHandler(){
+  for (uint8_t i = 0; i < SIMULTANEOUS_CONTROL_NUMBER; i++)
+    pinControlHandler(i);
 }
